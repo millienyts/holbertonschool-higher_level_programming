d1fd7613339bf6c04746b002c9516b9f4df7968b
millienytorressanta <tmillieny@gmail.com>
2024-01-19 01:58:18 -0400

    python first try

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for num in range(100):
-    print(f"{num:02}", end=", " if num < 99 else "\n" if num == 99 else "")
+    print("{:02d}".format(num), end=", " if num < 99 else "\n")
