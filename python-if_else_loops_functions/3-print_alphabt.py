45d8422819628dda519022bd7498693094d6e405
millienytorressanta <tmillieny@gmail.com>
2024-01-19 01:48:35 -0400

    python first try

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for char in range(ord('a'), ord('z') + 1):
-    if chr(char) not in ['q', 'e']:
-        print(chr(char), end='')
+for letter in range(97, 123):
+    if letter != 101 and letter != 113:
+        print('{}'.format(chr(letter)), end='')
--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
-for number in range(99 + 1):
-    print(f"{number} = 0x{number:x}")
+for number in range(0, 99):
+    print(f"{number} = "'{}'.format(hex(number)))
