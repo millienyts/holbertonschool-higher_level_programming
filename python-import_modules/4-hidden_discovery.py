a47cbe95db59ceb6f7aeb8c39e1ee7d6a9d9f1f7
millienytorressanta <tmillieny@gmail.com>
2024-01-22 20:11:57 -0500

    python first codes

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
-    my_list = dir(hidden_4)
-    max_len = len(my_list)
-    for i in range(0, max_len):
-        string = my_list[_]
-        if string[:2] != "__" != string[-2:]:
-            print(string)
+    names = dir(hidden_4)
+
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
