9f45d0da6d50094d73f2af794ed4396c5e2b8949
millienytorressanta <tmillieny@gmail.com>
2024-01-23 23:26:34 -0400

    changes

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
-        return my_list
-    new_list = my_list.copy()
-    del new_list[idx]
+        return my_list.copy()
+    new_list = [val for i, val in enumerate(my_list) if i != idx]
     return new_list
 
 # Test the function
