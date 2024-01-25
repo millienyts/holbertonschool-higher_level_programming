008c4197e94b824f5f20092ac3e1ecc6f6e93a8f
millienytorressanta <tmillieny@gmail.com>
2024-01-24 23:40:12 -0400

    changes for checkers

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    return [num % 2 == 0 for num in my_list]
-
-# Test the function
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
-
-for i in range(len(list_result)):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    new_list = my_list[:]
+    for idx in range(len(my_list)):
+        if (my_list[idx] % 2) != 0:
+            new_list[idx] = False
+        else:
+            new_list[idx] = True
+    return new_list
