6abfb3ffd2caac196d9cdd6e9d35f8463129d752
millienytorressanta <tmillieny@gmail.com>
2024-01-29 20:38:33 -0400

    Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,19 +2,40 @@
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
-     roman_to_int = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
     }
+
     result = 0
-    for i in range(len(roman_string)):
-        if i > 0 and roman_to_int[roman_string[i]] > roman_to_int[roman_string[i - 1]]:
-            result += roman_to_int[roman_string[i]] - 2 * roman_to_int[roman_string[i - 1]]
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman_numerals[char]
+
+        if value < prev_value:
+            result -= value
         else:
-            result += roman_to_int[roman_string[i]]
+            result += value
+
+        prev_value = value
+
     return result
+
+# Example usage:
+if __name__ == "__main__":
+    roman_number = "X"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "VII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "IX"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "LXXXVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "DCCVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
