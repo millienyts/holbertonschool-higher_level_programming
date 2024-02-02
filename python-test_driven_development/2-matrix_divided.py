1d57c3614fbe9217b92f5f97a84874c26601e981
millienytorressanta <tmillieny@gmail.com>
2024-02-01 22:32:16 -0400

    Matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
-"""
-Module to print "My name is <first name> <last name>"
-"""
+"""This module provides a fucntion to divide matrix,
+and store divided values in a lists of lists."""
 
 
-def say_my_name(first_name, last_name=""):
-    """
-    Function to print "My name is <first name> <last name>"
-    
-    Args:
-        first_name (str): First name
-        last_name (str): Last name (default is empty)
-    
-    Raises:
-        TypeError: If first_name or last_name is not a string
-    """
-    if type(first_name) is not str:
-        raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
-        raise TypeError("last_name must be a string")
+def matrix_divided(matrix, div):
+    """This function divides every integer"""
+    _matrix1 = []
+    _matrix2 = []
+    str1 = "matix must be a (list of list) of integers/floats"
+    num = 0
+    idx = 0
 
-    print("My name is {} {}".format(first_name, last_name))
+    for _list in matrix:
+        if type(_list) != list:
+            raise TypeError()
+        for i in _list:
+            if type(i) != int and type(i) != float:
+                raise TypeError(str1)
+
+        if type(div) != int and type(div) != float:
+            raise TypeError("div must be a number")
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+
+    if len(matrix[0]) != len(matrix[1]):
+        raise TypeError("Each row of the matrix must be the same size")
+
+    for j in matrix:
+        for i in j:
+            num = i / div
+            if idx == 0:
+                _matrix1.append(round(num, 2))
+            elif idx == 1:
+                _matrix2.append(round(num, 2))
+        idx += 1
+    matrix = [_matrix1, _matrix2]
+    return matrix
