d90eff177459f893de35da14faaa3e57ab261db3
millienytorressanta <tmillieny@gmail.com>
2024-02-08 02:38:47 -0400

    python inheritance

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""
+Append After Module
+"""
+
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Inserts new_string after each line containing search_string
+    Inserts a line of text to a file, after each line containing a specific string
     """
-    with open(filename, mode='r', encoding='utf-8') as f:
+    with open(filename, mode='r+') as f:
         lines = f.readlines()
-
-    with open(filename, mode='w', encoding='utf-8') as f:
+        f.seek(0)
         for line in lines:
             f.write(line)
             if search_string in line:
--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
+"""
+Pascal Triangle Module
+"""
+
 
 def pascal_triangle(n):
     """
-    Generates Pascal's triangle up to the nth row
+    Returns a list of lists of integers representing the Pascal’s triangle of n
     """
-    triangle = []
-
     if n <= 0:
-        return triangle
+        return []
 
+    triangle = []
     for i in range(n):
-        row = [1] * (i + 1)
-        if i >= 2:
-            for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        row = [1]
+        if triangle:
+            last_row = triangle[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
         triangle.append(row)
-
     return triangle
+
+
+if __name__ == "__main__":
+    n = 5
+    for row in pascal_triangle(n):
+        print(row)
