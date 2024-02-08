df7a53676cd9ad84594691bff15fc8b852fb6ba4
millienytorressanta <tmillieny@gmail.com>
2024-02-08 00:01:42 -0400

    python inheritance

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
+"""100-my_int module"""
+
 class MyInt(int):
-    """A class representing an integer with inverted equality operators."""
+    """A class representing an integer with inverted equality operators.
+
+    Attributes:
+        int: The integer value.
+
+    Methods:
+        __eq__(self, other): Overrides the equality operator.
+        __ne__(self, other): Overrides the inequality operator.
+    """
+    
     def __eq__(self, other):
         """Overrides the equality operator."""
         return super().__ne__(other)
