a8d019e6668c414c83c156e013b5ee8f6d61361a
millienytorressanta <tmillieny@gmail.com>
2024-02-09 10:08:11 -0400

    python inheritance

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -29,4 +29,25 @@ def main():
     Main function to read stdin and compute metrics
     """
     count = 0
-    total_size
+    total_size = 0
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            status_code, file_size = parse_line(line)
+            if status_code is None or file_size is None:
+                continue
+            total_size += file_size
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+
+            if count % 10 == 0:
+                print_metrics(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_metrics(total_size, status_codes)
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
