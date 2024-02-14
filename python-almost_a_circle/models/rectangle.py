e446c8aec5982cf1b136c3258d50c36c2034de57
millienytorressanta <tmillieny@gmail.com>
2024-02-14 00:27:07 -0400

    models

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,42 +1,62 @@
 #!/usr/bin/python3
-"""
-Rectangle module
-"""
+"""comment"""
+
 
 from models.base import Base
 
-
 class Rectangle(Base):
-    """
-    Rectangle class
-    """
-
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Initialize Rectangle class
-        """
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-    def to_dictionary(self):
-        """
-        Return dictionary representation of Rectangle instance
-        """
-        return {
-            'id': self.id,
-            'width': self.width,
-            'height': self.height,
-            'x': self.x,
-            'y': self.y
-        }
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    def area(self):
+        return self.width * self.height
+
+    def display(self):
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
-        """
-        Assign arguments to attributes
-        """
         if args:
             attrs = ['id', 'width', 'height', 'x', 'y']
             for attr, value in zip(attrs, args):
@@ -45,24 +65,12 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-    def __str__(self):
-        """
-        String representation of Rectangle instance
-        """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y, self.width, self.height)
+    def to_dictionary(self):
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
 
-    def display(self):
-        """
-        Print Rectangle instance with '#'
-        """
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
-
-    def area(self):
-        """
-        Return area of Rectangle
-        """
-        return self.width * self.height
