e3ea67a83c86a36dbb55a5d41523d8eaa81bae65
millienytorressanta <tmillieny@gmail.com>
2024-02-16 09:21:08 -0400

    models

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -34,24 +34,44 @@ class Base:
         if list_objs is None:
             list_objs = []
 
-        filename = str(cls.__name__) + ".json"
+        filename = f"{cls.__name__}.json"
         json_list = [obj.to_dictionary() for obj in list_objs]
         json_str = cls.to_json_string(json_list)
         with open(filename, "w") as file:
             file.write(json_str)
 
-     @classmethod
+    @staticmethod
+    def from_json_string(json_string):
+        a_list = []
+        if json_string is None:
+            return a_list
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """comment"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
     def load_from_file(cls):
         """comment"""
         filename = f"{cls.__name__}.json"
-        if not filename:
+        try:
+            obj = []
+            with open(filename, "r") as file:
+                data = file.read()
+                if data:
+                    json_list = cls.from_json_string(data)
+                    obj = [
+                        cls.create(**dictionary)
+                        for dictionary in json_list
+                    ]
+                    return obj
+        except FileNotFoundError:
             return []
-        with open(filename, "r") as file:
-            data = file.read()
-            if not data:
-                return []
-            else:
-                return [
-                    cls.from_json_string(json.dumps(obj))
-                    for obj in json.loads(data)
-                ]
