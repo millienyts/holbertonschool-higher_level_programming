7f6b3ef704cee0c3e86404f0202f5e55f8545493
millienytorressanta <tmillieny@gmail.com>
2024-02-16 10:47:21 -0400

    models

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,4 +1,3 @@
-/test_rectangle.py 
 #!/usr/bin/python3
 """comment"""
 
@@ -20,22 +19,6 @@ class TestRectangle(unittest.TestCase):
     def test_rectangle_string_height(self):
         with self.assertRaises(TypeError):
             Rectangle(1, "2")
-
-    def test_rectangle_string_x(self):
-        with self.assertRaises(TypeError):
-            Rectangle(1, 2, "3")
-
-    def test_rectangle_string_y(self):
-        with self.assertRaises(TypeError):
-            Rectangle(1, 2, 3, "4")
-
-    def test_rectangle_five_arguments(self):
-        r = Rectangle(1, 2, 3, 4, 5)
-        self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 2)
-        self.assertEqual(r.x, 3)
-        self.assertEqual(r.y, 4)
-        self.assertEqual(r.id, 5)
 
 if __name__ == '__main__':
     unittest.main()
