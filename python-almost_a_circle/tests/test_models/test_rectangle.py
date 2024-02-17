f95442093c2c96017696307bd70a79506e5a942e
millienytorressanta <tmillieny@gmail.com>
2024-02-16 21:50:58 -0400

    models

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -4,21 +4,59 @@
 
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
 
 class TestRectangle(unittest.TestCase):
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
 
     def test_rectangle_creation(self):
         r = Rectangle(1, 2)
         self.assertEqual(r.width, 1)
         self.assertEqual(r.height, 2)
 
-    def test_rectangle_string_width(self):
+    def test_rectangle_string(self):
         with self.assertRaises(TypeError):
             Rectangle("1", 2)
-
-    def test_rectangle_string_height(self):
         with self.assertRaises(TypeError):
             Rectangle(1, "2")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "3")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+
+    def test_rectangle_five_arguments(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 3)
+        self.assertEqual(r.y, 4)
+        self.assertEqual(r.id, 5)
+
+    def test_rectangle_negative_numbers(self):
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-1, 2)
+        with self.assertRaises(ValueError):
+            r2 = Rectangle(1, -2)
+        with self.assertRaises(ValueError):
+            r3 = Rectangle(1, 2, -3)
+        with self.assertRaises(ValueError):
+            r4 = Rectangle(1, 2, 3, -4)
+
+    def test_rectangle_zero_values(self):
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(0, 2)
+        with self.assertRaises(ValueError):
+            r2 = Rectangle(1, 0)
+
+    def test_area_method_exists(self):
+        r1 = Rectangle(5, 10)
+        self.assertEqual(r1.area(), 50)
+
+    def test_str_method_exist(self):
+        r1 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r1.__str__(), '[Rectangle] (1) 3/4 - 1/2')
 
 if __name__ == '__main__':
     unittest.main()
