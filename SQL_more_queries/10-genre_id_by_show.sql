032eb40e9e672bcb976ba6700c77446c2342e6c8
millienytorressanta <tmillieny@gmail.com>
2024-03-07 23:02:14 -0400

    fist try in SQL

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Show liked genres LEFT JOIN
+SELECT b.title, a.genre_id 
+FROM tv_show_genres a 
+LEFT JOIN tv_shows b 
+ON a.show_id = b.id 
+ORDER BY b.title, a.genre_id ASC;
---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- List all cities of California in the database(subquery)
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name="California")
+ORDER BY id ASC;
