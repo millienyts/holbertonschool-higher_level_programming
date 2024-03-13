e006e03229b49e828976a47da2c61d8c6fbbc84e
millienytorressanta <tmillieny@gmail.com>
2024-03-12 20:03:09 -0400

    mapping

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
 """
-    script that lists all cities from the database hbtn_0e_4_usa
+List all cities from the db
+Takes 3 arguments
 """
-
-
-import sys
+from sys import argv
 import MySQLdb
 
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        database=sys.argv[3]
-    )
+    db = MySQLdb.connect(host="localhost",
+                        port=3306, user=argv[1], 
+                        passwd=argv[2], db=argv[3])
 
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name\
-                    FROM cities\
-                    INNER JOIN states ON cities.state_id = states.id\
-                    ORDER BY cities.id ASC")
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
 
-    rows = cursor.fetchall()
+    lst = cur.fetchall()
+    for r in lst:
+        print(r)
 
-    for row in rows:
-        print(row)
-
-    cursor.close()
+    cur.close()
     db.close()
--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
 """
-    script that takes in the name of a state as an argument and lists all
-    cities of that state, using the database hbtn_0e_4_usa
+Takes in name of state as argument and list
+all cities of that state
 """
-
-
-import sys
+from sys import argv
 import MySQLdb
 
 
 if __name__ == "__main__":
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        database=sys.argv[3]
-    )
-
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.name FROM cities\
-                    INNER JOIN states ON cities.state_id = states.id\
-                    WHERE states.name = BINARY %s", (sys.argv[4],))
-
-    rows = cursor.fetchall()
-
-    total_rows = len(rows)
-
-    for row in range(total_rows):
-        print(rows[row][0], end="")
-        if row < total_rows - 1:
-            print(", ", end="")
-    print()
-
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", 
+                        port=3306, user=argv[1],
+                        passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    check = (argv[4], )
+    cur.execute("SELECT * FROM cities JOIN states\
+    ON cities.state_id = states.id WHERE states.name = %s\
+    ORDER BY cities.id ASC", check)
+    lst = cur.fetchall()
+    cities = []
+    for r in lst:
+        if r[4] == check[0]:
+            cities.append(r[2])
+    print(', '.join(cities))
+    cur.close()
     db.close()
