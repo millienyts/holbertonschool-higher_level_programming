42f8956f6e6450214b8090eb5e8623ce74b6eee9
millienytorressanta <tmillieny@gmail.com>
2024-05-23 23:32:23 -0400

    dom manipilation

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('red_header').addEventListener('click', function() {
+      document.querySelector('header').classList.add('red');
+    });
+  });
